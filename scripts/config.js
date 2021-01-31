774000a8a546589ac981a7a3d9f6bf50866b60f2
Xavion <xavion333@gmail.com>
2021-01-31 22:24:40 +1100

    Initial commit
    Inserts initial code and data

---
+++ scripts/config.js
@@ -0,0 +1,34 @@
+// Namespace SpheresSheet Configuration Values
+export const SpheresSheet = {};
+
+/**
+ * The set of Combat Spheres
+ * @type {Object}
+ */
+SpheresSheet.combatSpheres = {
+  alchemy: "SpheresSheet.SphereAlchemy",
+  athletics: "SpheresSheet.SphereAthletics",
+  barrage: "SpheresSheet.SphereBarrage",
+  barroom: "SpheresSheet.SphereBarroom",
+  beastmastery: "SpheresSheet.SphereBeastmastery",
+  berserker: "SpheresSheet.SphereBerserker",
+  boxing: "SpheresSheet.SphereBoxing",
+  brute: "SpheresSheet.SphereBrute",
+  dualWielding: "SpheresSheet.SphereDualWielding",
+  duelist: "SpheresSheet.SphereDuelist",
+  equipment: "SpheresSheet.SphereEquipment",
+  fencing: "SpheresSheet.SphereFencing",
+  gladiator: "SpheresSheet.SphereGladiator",
+  guardian: "SpheresSheet.SphereGuardian",
+  lancer: "SpheresSheet.SphereLancer",
+  leadership: "SpheresSheet.SphereLeadership",
+  openHand: "SpheresSheet.SphereOpenHand",
+  scoundrel: "SpheresSheet.SphereScoundrel",
+  scout: "SpheresSheet.SphereScout",
+  shield: "SpheresSheet.SphereShield",
+  sniper: "SpheresSheet.SphereSniper",
+  tech: "SpheresSheet.SphereTech",
+  trap: "SpheresSheet.SphereTrap",
+  warleader: "SpheresSheet.SphereWarleader",
+  wrestling: "SpheresSheet.SphereWrestling",
+}
---
+++ scripts/sheet.js
@@ -0,0 +1,98 @@
+
+// Import Modules
+import { SpheresSheet } from "./config.js";
+
+import { ItemPF } from "../../../systems/pf1/module/item/entity.js";
+import { ActorSheetPF } from "../../../systems/pf1/module/actor/sheets/base.js";
+import { ItemSheetPF } from "../../../systems/pf1/module/item/sheets/base.js";
+import { ActorSheetPFCharacter } from "../../../systems/pf1/module/actor/sheets/character.js";
+import { ActorSheetPFNPC } from "../../../systems/pf1/module/actor/sheets/npc.js";
+
+function injectIntoClass(classObj, functionName, prior, injection) {
+  console.log(`SpheresSheet | Injecting into ${classObj.name}.${functionName}`);
+  var funcText = classObj.prototype[functionName].toString();
+  var insertionPoint = funcText.indexOf(prior) + prior.length;
+  if (insertionPoint == -1) {
+    console.log('SpheresSheet | Failed injection due to not finding prior');
+    return;
+  }
+  var modifiedFunction = funcText.slice(0, insertionPoint) + injection + funcText.slice(insertionPoint);
+  classObj.prototype[functionName] = eval('(function ' + modifiedFunction + ')');
+}
+
+Hooks.once('init', async function() { 
+  console.log('spheres-sheet | Initializing spheres sheet');
+
+  CONFIG.SpheresSheet = SpheresSheet;
+
+  // // Add talent feat type
+  CONFIG.PF1.featTypes['combatTalent'] = "SpheresSheet.FeatTypeCombatTalent";
+
+  // Inject talents category into feats
+  injectIntoClass(ActorSheetPF, '_prepareItems', '"feat-type": "classFeat" },\n      },',
+    `\n      combatTalent: {
+        label: game.i18n.localize("SpheresSheet.CombatTalentPlural"),
+        items: [],
+        canCreate: true,
+        hasActions: true,
+        dataset: { type: "feat", "type-name": game.i18n.localize("SpheresSheet.FeatTypeCombatTalent"), "feat-type": "combatTalent" },
+      },`);
+
+  injectIntoClass(ItemSheetPF, '_getItemProperties', 'props.push(labels.featType);\n',
+    `      if ( item.data.featType == "combatTalent" && this.object.getFlag("spheres-sheet", "sphere")) {
+        props.push(game.i18n.localize(this.object.getFlag("spheres-sheet", "sphere")));
+      }\n`);
+
+  hookRenderers();
+});
+
+function hookRenderers() {
+  Hooks.on('renderActorSheet', (characterSheet, html, data) => {
+    console.log('spheres-sheet | Inserting spheres column');
+    const sphereHeader = $(`<div class="item-detail item-sphere"><span>${game.i18n.localize('SpheresSheet.Sphere')}</span></div>`);
+
+    var featsGroups = html.find('.feats-group.flexcol');
+    var wantedName = game.i18n.localize("SpheresSheet.CombatTalentPlural");
+    for (var i = 0; i < featsGroups.length; i++) {
+        if ($(featsGroups[i]).find('li.inventory-header h3').text() == wantedName) {
+          var combatTalentsGroup = $(featsGroups[i]);
+          combatTalentsGroup.find('li.inventory-header h3').after(sphereHeader);
+
+          combatTalentsGroup.find('li.item').each((index, element) => {
+            var talentId = $(element).data('item-id');
+            var sphere = characterSheet.object.items.get(talentId).getFlag('spheres-sheet', 'sphere');
+            if (!sphere) sphere = '';
+            var sphereCol = $('<div class="item-detail item-sphere"><span>' + game.i18n.localize(sphere) + '</span></div>')
+            $(element).find('div.item-name').after(sphereCol);
+          });
+        }
+    }
+  });
+
+  Hooks.on('renderItemSheetPF', (itemSheet, html, data) => {
+    if (itemSheet.object.data.data.featType === 'combatTalent') {
+      var sphere = itemSheet.object.getFlag('spheres-sheet', 'sphere');
+      if (!sphere) sphere = CONFIG.SpheresSheet.combatSpheres.equipment;
+      const sphereDropdown = $(`<div class="form-group">
+        <label>${game.i18n.localize('SpheresSheet.Sphere')}</label>
+        <select name="data.sphere">
+          ${Object.values(CONFIG.SpheresSheet.combatSpheres)
+            .map(element => `<option value="${element}"${element == sphere ? ' selected' : ''}>${game.i18n.localize(element)}</option>`).join('\n')}
+        </select>
+      </div>`);
+      html.find('div.tab.details > div:nth-child(2)').after(sphereDropdown);
+    }
+  });
+
+  Hooks.on('updateOwnedItem', (actorSheet, item, change, diff, _id) => {
+    if ('data' in change && 'sphere' in change.data) {
+      actorSheet.items.get(change._id).setFlag('spheres-sheet', 'sphere', change.data.sphere);
+    }
+  });
+
+  Hooks.on('updateItem', (item, change, diff, _id) => {
+    if ('data' in change && 'sphere' in change.data) {
+      item.setFlag('spheres-sheet', 'sphere', change.data.sphere);
+    }
+  });
+}
